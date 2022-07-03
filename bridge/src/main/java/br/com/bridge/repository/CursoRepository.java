61d607fbeb9cb81892f92e8f1ea295a8f47a6daa
Caroline Moraes da Cruz <carolmoraescruz@gmail.com>
2022-07-03 13:39:38 -0300

    imp: criando camada repository para Cursos

---
+++ bridge/src/main/java/br/com/bridge/repository/CursoRepository.java
@@ -0,0 +1,11 @@
+package br.com.bridge.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.bridge.domain.entity.Curso;
+
+@Repository
+public interface CursoRepository extends JpaRepository<Curso, Long>{
+
+}
