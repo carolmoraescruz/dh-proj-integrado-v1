750f9db590b4927b6a091022bd03b84bb7034c53
Caroline Moraes da Cruz <carolmoraescruz@gmail.com>
2022-07-03 16:53:43 -0300

    imp: criando camada controller para Cursos

---
+++ bridge/src/main/java/br/com/bridge/controller/CursoController.java
@@ -0,0 +1,51 @@
+package br.com.bridge.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.bridge.domain.entity.Curso;
+import br.com.bridge.service.CursoService;
+
+@RestController
+@RequestMapping("/curso")
+public class CursoController {
+	
+	@Autowired
+	CursoService service;
+	
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Curso> findAll() {
+		return service.findAll();
+	}
+	
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Curso findById(@PathVariable("id") Long id) {
+		return service.findById(id);
+	}
+	
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Curso create(@RequestBody Curso curso) {
+		return service.insert(curso);
+	}
+	
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Curso update(@RequestBody Curso curso) {
+		return service.update(curso);
+	}
+	
+	@DeleteMapping(value="/{id")
+	public void delete(@PathVariable("id") Long id) {
+		service.delete(id);
+	}
+
+}
